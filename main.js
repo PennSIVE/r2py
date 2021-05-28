335c2f9f09cb3b15a896a1001837b54c0e35f2ca
Terf <tim.terf@gmail.com>
2021-05-28 10:07:36 -0400

    rewrote in js so it could be native js gh action

---
+++ main.js
@@ -0,0 +1,58 @@
+const fs = require('fs-extra');
+const convert = require('./src/r2py.js');
+const path = require('path');
+const os = require('os');
+const execSync = require('child_process').execSync;
+const replace = require('replace-in-file');
+const fetch = require('node-fetch');
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+
+async function main() {
+    try {
+        const username = core.getInput('username')
+        const repo = core.getInput('repo')
+        const email = core.getInput('email')
+        const password = core.getInput('password')
+        const pat = core.getInput('pat')
+        await fetch('https://api.github.com/user/repos', {
+            method: 'POST',
+            headers: {
+                'Authorization': `token ${pat}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name: repo })
+        });
+        fs.mkdtemp(path.join(os.tmpdir(), 'r2py-'), (err, folder) => {
+            if (err) throw err;
+            process.chdir(folder);
+            execSync(`git config --global user.email ${email}`)
+            execSync(`git config --global user.name ${username}`)
+            execSync('git config --global init.defaultBranch main')
+            execSync('git init')
+            fs.copySync(`${__dirname}/boilerplate/`, '.')
+            fs.moveSync('PackageName', repo)
+            replace.sync({
+                files: 'setup.cfg',
+                from: /PackageName/g,
+                to: repo,
+            })
+            fs.writeFileSync("README.md", `Auto generated wrapper for ${repo}`)
+            const man_files = path.join(process.env.GITHUB_WORKSPACE, 'man')
+            fs.writeFileSync(`${repo}/__init__.py`, convert(repo, fs.readdirSync(man_files).map(x => `${man_files}/` + x)));
+            execSync('git add .')
+            execSync('git commit -m "first commit"')
+            execSync(`git remote add origin https://${username}:${password}@github.com/${username}/${repo}.git`)
+            execSync('git branch -M main')
+            execSync(`git push https://${username}:${password}@github.com/${username}/${repo}.git`)
+        });
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+main();
+
+// console.log(convert('WhiteStripe', fs.readdirSync('../WhiteStripe/man/').map(x => '../WhiteStripe/man/' + x)))
+// convert('WhiteStripe', ['../WhiteStripe/man/whitestripe.Rd'])
---
+++ src/r2py.js
@@ -0,0 +1,118 @@
+const fs = require('fs');
+
+const convert = (packageName, RdFiles, description = null) => {
+    const escapeRegExp = (string) => { // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping
+        return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+    }
+    const trimByChar = (string, character) => {
+        const trimmed = string.trim()
+        const arr = Array.from(trimmed);
+        let start = 0, end = arr.length;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] !== character) {
+                start = i;
+                break;
+            }
+        }
+        for (let i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] !== character) {
+                end = i;
+                break;
+            }
+        }
+        return (trimmed === character) ? '' : trimmed.substring(start, end + 1)
+    }
+    const special_defaults = (value) => {
+        switch (value) {
+            case 'NA':
+            case '':
+                return "r('NA')"
+            case 'NULL':
+                return "r('NULL')"
+            case 'FALSE':
+            case 'F':
+                return "False"
+            case 'TRUE':
+            case 'T':
+                return "True"
+            default:
+                if (!isNaN(value)) {
+                    return value
+                }
+                if (/^(\"|')[A-Za-z]+(\"|')$/.test(value)) { // quoted string
+                    return value
+                }
+                return [value] // return as singleton array to mark for special processing
+        }
+    }
+    const parse_args = (usage, arg_names, method_name) => {
+        if (!arg_names) {
+            return {};
+        }
+        const args = {};
+        const arg_list = trimByChar(usage.replace(`${method_name}(`), ')');
+
+        for (let i = 0; i < arg_names.length; i++) {
+            if (arg_names[i] !== '...') {
+                const def = (i < arg_names.length - 1) ? new RegExp(`${escapeRegExp(arg_names[i])}(.+?)${escapeRegExp(arg_names[i + 1])}`, 'gms').exec(arg_list)[1] : arg_list.split(arg_names[i], 2)[1];
+                args[arg_names[i]] = special_defaults(trimByChar(trimByChar(def, '='), ','));
+            }
+
+        }
+        return args
+    }
+
+    const methods = [];
+    for (const RdFile in RdFiles) {
+        const f = RdFiles[RdFile];
+        const contents = fs.readFileSync(f, 'utf8');
+        // match \usage{ then everything until } https://stackoverflow.com/a/7124976/2624391
+        const re = /\\usage\s*\{(.+?(?=\}))\}/gs;
+        const usage = re.exec(contents)[1];
+
+        const arg_names = contents.match(/\\item\{.+?(?=\})/gs)?.map(x => x.replace("\\item{", ''));
+        const method_name = contents.match(/\\name\{.+?(?=\})/gs).map(x => x.replace("\\name{", ''))[0];
+
+        const args = parse_args(usage, arg_names, method_name);
+
+        const buffer1 = [], buffer2 = [], buffer3 = [];
+        for (const arg in args) {
+            const def = args[arg];
+            const complex_default = (def.constructor === Array);
+            buffer1.push(`${arg.replaceAll('.', '_')} = ${(complex_default) ? 'None' : def}`);
+            (!complex_default) ?
+                buffer2.push(`r.assign('${arg}', ${arg.replaceAll('.', '_')})`)
+            :
+                buffer3.push(`if ${arg.replaceAll('.', '_')} is None:\n\t\t${arg.replaceAll('.', '_')} = r('${def[0]}')\n\tr.assign('${arg}', ${arg.replaceAll('.', '_')})`)
+        }
+        const signature = `def ${method_name.replaceAll('.', '_')}(${buffer1.join(', ')}):\n\t`
+        const assignments = (buffer2.length > 0) ? buffer2.join("\n\t") : ''
+        const validated_assignments = (buffer3.length > 0) ? "\n\t" + buffer3.join("\n\t") : ''
+        const ret = (Object.keys(args).length === 0) ? `return r('${method_name}')\n` : `\n\treturn r('${method_name}(${Object.keys(args).map(x => x + '=' + x).join(', ')})')\n`;
+
+        methods.push(signature + assignments + validated_assignments + ret)
+
+    }
+    // console.log(methods.join("\n"))
+    return "import os\n" +
+        "from rpy2.robjects import r, numpy2ri, pandas2ri\n" +
+        "from rpy2.robjects.packages import importr\n" +
+        "numpy2ri.activate()\n" +
+        "pandas2ri.activate()\n" +
+        `importr('${packageName}')\n\n` +
+        "neurobase = importr('neurobase')\n\n" +
+        "def readnii(file):\n" +
+        "\treturn neurobase.readnii(file)\n" +
+        "def writenii(data, file):\n" +
+        "\timport nibabel as nib\n" +
+        "\timport numpy as np\n" +
+        "\tif type(data) is np.ndarray:\n" +
+        "\t\tnew_image = nib.Nifti1Image(data, affine=np.eye(4))\n" +
+        "\t\treturn new_image.to_filename(file)\n" +
+        "\telse:\n" +
+        "\t\treturn neurobase.writenii(data, file)\n\n\n" +
+        methods.join("\n")
+
+}
+
+module.exports = convert
