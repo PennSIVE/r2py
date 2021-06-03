0cf724d39f75ccb5cf098ae6421698e747603392
Terf <tim.terf@gmail.com>
2021-06-03 14:01:45 -0400

    gh pass no longer needed as files uploaded through API

--- main.js
+++ main.js
@@ -4,25 +4,37 @@ const path = require('path');
 const os = require('os');
 const execSync = require('child_process').execSync;
 const replace = require('replace-in-file');
-const fetch = require('node-fetch');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+async function* getFiles(dir, blacklist = ['.git', 'node_modules', '__pycache__', '.DS_Store']) {
+    // https://stackoverflow.com/a/45130990/2624391
+    const dirents = await fs.readdir(dir, { withFileTypes: true });
+    for (const dirent of dirents) {
+        const res = path.resolve(dir, dirent.name);
+        if (blacklist.includes(dirent.name)) {
+            // return;
+        } else if (dirent.isDirectory()) {
+            yield* getFiles(res);
+        } else {
+            yield res;
+        }
+    }
+}
 
 async function main() {
     try {
         const username = core.getInput('username')
         const repo = core.getInput('repo')
         const email = core.getInput('email')
-        const password = core.getInput('password')
         const pat = core.getInput('pat')
-        await fetch('https://api.github.com/user/repos', {
-            method: 'POST',
-            headers: {
-                'Authorization': `token ${pat}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ name: `${repo}.py` })
+        const octokit = github.getOctokit(pat);
+        await octokit.request('POST /user/repos', {
+            name: `${repo}.py`,
+            auto_init: true, // needed if uploading via API rather than git push
+            has_issues: false,
+            has_projects: false,
+            has_wiki: false
         });
         fs.mkdtemp(path.join(os.tmpdir(), 'r2py-'), (err, folder) => {
             if (err) throw err;
@@ -38,14 +50,26 @@ async function main() {
                 from: /PackageName/g,
                 to: repo,
             })
-            fs.writeFileSync("README.md", `Auto generated wrapper for ${repo}`)
             const man_files = path.join(process.env.GITHUB_WORKSPACE, 'man')
             fs.writeFileSync(`${repo}/__init__.py`, convert(repo, fs.readdirSync(man_files).map(x => `${man_files}/` + x)));
-            execSync('git add .')
-            execSync('git commit -m "first commit"')
-            execSync(`git remote add origin https://${username}:${password}@github.com/${username}/${repo}.py.git`)
-            execSync('git branch -M main')
-            execSync(`git push https://${username}:${password}@github.com/${username}/${repo}.py.git`)
+            // execSync('git add .')
+            // execSync('git commit -m "first commit"')
+            // execSync(`git remote add origin https://${username}:${password}@github.com/${username}/${repo}.py.git`)
+            // execSync('git branch -M main')
+            // execSync(`git push https://${username}:${password}@github.com/${username}/${repo}.py.git`)
+            // instead of `git push` (which req's a password), upload each file individually via API
+            (async () => {
+                for await (const f of getFiles(folder)) {
+                    await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+                        owner: username,
+                        repo: `${repo}.py`,
+                        path: path.relative(folder, f),
+                        message: 'BOT: PennSIVE/r2py run',
+                        content: fs.readFileSync(f, { encoding: 'base64' })
+                    })
+
+                }
+            })();
         });
     } catch (error) {
         core.setFailed(error.message);
@@ -53,6 +77,3 @@ async function main() {
 }
 
 main();
-
-// console.log(convert('WhiteStripe', fs.readdirSync('../WhiteStripe/man/').map(x => '../WhiteStripe/man/' + x)))
-// convert('WhiteStripe', ['../WhiteStripe/man/whitestripe.Rd'])
